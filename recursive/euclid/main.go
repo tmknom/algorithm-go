c68f1ab109569b1d4ca4b153cc2096d6528e45e0
tmknom <tmk.nom@gmail.com>
2021-04-16 08:26:24 +0900

    ユーティリティのインタフェース変更に伴う修正

--- recursive/euclid/main.go
+++ recursive/euclid/main.go
@@ -6,14 +6,14 @@ import (
 )
 
 func GCD(m int, n int, indent int) int {
-	util.IndentPrintf(indent, "call GCD(%d, %d)\n", m, n)
+	util.CallPrintf(indent, "GCD(%d, %d)\n", m, n)
 	if n == 0 {
-		util.IndentPrintf(indent, "return GCD(%d, %d) = %d\n", m, n, m)
+		util.ReturnPrintf(indent, "GCD(%d, %d) = %d\n", m, n, m)
 		return m
 	}
 
 	result := GCD(n, m%n, indent+1)
-	util.IndentPrintf(indent, "return GCD(%d, %d) = %d\n", m, n, result)
+	util.ReturnPrintf(indent, "GCD(%d, %d) = %d\n", m, n, result)
 	return result
 }
 
--- recursive/partial_sum/main.go
+++ recursive/partial_sum/main.go
@@ -11,34 +11,34 @@ const INF = 1 << 60
 // 1. inputs[N-1]を含まない：inputs[0]...inputs[N-2]の部分和「w」が存在する
 // 2. inputs[N-1]を含む：inputs[0]...inputs[N-2]の部分和「w-inputs[N-1]」が存在する
 func PartialSum(i int, w int, inputs *[]int, indent int) bool {
-	util.IndentPrintf(indent, "call PartialSum(%v, %v)\n", i, w)
+	util.CallPrintf(indent, "PartialSum(%v, %v)\n", i, w)
 
 	// ベースケース
 	// ゼロ個の配列の総和がゼロなら部分和が存在する
 	if i == 0 {
 		if w == 0 {
-			util.IndentPrintf(indent, "return PartialSum(%v, %v) = true\n", i, w)
+			util.ReturnPrintf(indent, "PartialSum(%v, %v) = true\n", i, w)
 			return true
 		} else {
-			util.IndentPrintf(indent, "return PartialSum(%v, %v) = false\n", i, w)
+			util.ReturnPrintf(indent, "PartialSum(%v, %v) = false\n", i, w)
 			return false
 		}
 	}
 
 	// 1. inputs[N-1]を含まない：inputs[0]...inputs[N-2]の部分和「w」が存在する
 	if PartialSum(i-1, w, inputs, indent+1) {
-		util.IndentPrintf(indent, "return PartialSum(%v, %v) = true\n", i-1, w)
+		util.ReturnPrintf(indent, "PartialSum(%v, %v) = true\n", i-1, w)
 		return true
 	}
 
 	// 2. inputs[N-1]を含む：inputs[0]...inputs[N-2]の部分和「w-inputs[N-1]」が存在する
 	if PartialSum(i-1, w-(*inputs)[i-1], inputs, indent+1) {
-		util.IndentPrintf(indent, "return PartialSum(%v, %v) = true\n", i-1, w-(*inputs)[i-1])
+		util.ReturnPrintf(indent, "PartialSum(%v, %v) = true\n", i-1, w-(*inputs)[i-1])
 		return true
 	}
 
 	// 1・2番の条件に合致しない
-	util.IndentPrintf(indent, "return PartialSum(%v, %v) = false\n", i, w)
+	util.ReturnPrintf(indent, "PartialSum(%v, %v) = false\n", i, w)
 	return false
 }
 
@@ -50,9 +50,9 @@ func run(w int, inputs *[]int) {
 func main() {
 	run(4, &[]int{1, 2, 3})
 
-	//fmt.Println("")
-	//run(11, &[]int{5, 10, 6, 100, 23})
-	//
-	//fmt.Println("")
-	//run(17, &[]int{5, 10, 6, 100, 23})
+	fmt.Println("")
+	run(11, &[]int{5, 10, 6, 100, 23})
+
+	fmt.Println("")
+	run(17, &[]int{5, 10, 6, 100, 23})
 }
